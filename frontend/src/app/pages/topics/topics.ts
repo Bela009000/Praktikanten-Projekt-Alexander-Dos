3ac49e26a799536030c71c621d49a3652c7c77bf
09Alexnd3r <alexanderdossantospaim@gmail.com>
2026-09-15 11:37:19 +0200

    topics_finished

--- frontend/src/app/pages/topics/topics.ts
+++ frontend/src/app/pages/topics/topics.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
   selector: 'app-topics',
-  styleUrl: './topics.css',
+  standalone: true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './topics.html',
+  styleUrl: './topics.css'
 })
-export class Topics {}
+export class TopicsComponent {
+
+  topicName: string = '';
+
+  topics: string[] = [];
+
+  addTopic() {
+
+    if (this.topicName.trim() === '') {
+      return;
+    }
+
+    this.topics.push(this.topicName);
+
+    this.topicName = '';
+  }
+
+  deleteTopic(index: number) {
+
+    this.topics.splice(index, 1);
+
+  }
+}
+``
