9c93741cd0f8f2d6a4aeaa31a9ec92e920094493
09Alexnd3r <alexanderdossantospaim@gmail.com>
2026-09-15 08:16:30 +0200

    login

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class LoginComponent {
 
+  username: string = '';
+  password: string = '';
+
+  login() {
+    console.log('Benutzername:', this.username);
+    console.log('Passwort:', this.password);
+  }
 }
